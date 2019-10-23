b09f80b3c95cb4f985dea2d90c389beb0f0a7bdf
Kanekiark28 <konarkrajmishra@gmail.com>
2019-10-22 19:36:12 -0700

    Updates

---
+++ precache-manifest.a8beae68aefde448c98cfeed52d89617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cbdf032fbee02e31a0aa729ea4543c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f6719fe8ef9bd323e776",
+    "url": "/react-portfolio/static/css/main.46374327.chunk.css"
+  },
+  {
+    "revision": "63f0b0ce62266a848932",
+    "url": "/react-portfolio/static/js/2.60064499.chunk.js"
+  },
+  {
+    "revision": "f6719fe8ef9bd323e776",
+    "url": "/react-portfolio/static/js/main.b7812e76.chunk.js"
+  },
+  {
+    "revision": "175a75cdfaf1231de18b",
+    "url": "/react-portfolio/static/js/runtime-main.98067e24.js"
+  },
+  {
+    "revision": "8d2769bd8e2ed5589c09ed69b7ed25eb",
+    "url": "/react-portfolio/static/media/back.8d2769bd.jpg"
+  },
+  {
+    "revision": "fd1d110ad99d6bdb3ecacc0d88327631",
+    "url": "/react-portfolio/static/media/profile.fd1d110a.jpg"
+  }
+]);
